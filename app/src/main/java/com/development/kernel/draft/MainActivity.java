fdb6c99891970e5d33daf93bd877936fa12d70e6
KerJen <kerjen01@gmail.com>
2016-12-04 15:22:36 +0300

    Создал проект, добавил OprionsMenu в приложение и создал класс MenuInspector, который будет его (меню) контролировать.

---
+++ app/src/main/java/com/development/kernel/draft/MainActivity.java
@@ -0,0 +1,30 @@
+package com.development.kernel.draft;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0,1,0,"Кнопка");
+        menu.add(0,2,0,"Строка");
+        menu.add(0,3,0,"Изображение");
+        menu.add(0,4,0,"Макетный холст");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/development/kernel/draft/MenuInspector.java
@@ -0,0 +1,15 @@
+package com.development.kernel.draft;
+
+import android.view.MenuItem;
+
+/**
+ * Created by Anton on 04.12.2016.
+ */
+
+public class MenuInspector {
+
+    public MenuInspector(MenuItem item)
+    {
+
+    }
+}
