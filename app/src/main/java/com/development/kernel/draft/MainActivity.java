107762df7f1b184f8f2a55fa23bafc2ccdabb705
KerJen <kerjen01@gmail.com>
2016-12-05 17:23:35 +0300

    Полностью реализовал класс ViewInspector, осталось его только обновлять, когда добавляем новые элементы для пользователя. Теперь в приложение спокойно можно изменять параметры textView и button (сложная задача, была путанница в тегах), теперь я ее исправил. В будущем, оптимизирую еще лучше код и добавлю imageView с возможностью загрузать в него фотографию

--- app/src/main/java/com/development/kernel/draft/MainActivity.java
+++ app/src/main/java/com/development/kernel/draft/MainActivity.java
@@ -2,30 +2,41 @@ package com.development.kernel.draft;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private LayoutParams layoutParams;
-    private LinearLayout linearLayout;
+    private ViewInspector viewInspector;
     private Button[] buttons;
     private TextView[] textViews;
     private int countOfButtons=0;
     private int countOfTextViews=0;
+    private int ID;
+    private boolean checkElement;
+    private EditText editText;
+    private Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //назначаем начальные настройки
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        linearLayout = (LinearLayout) findViewById(R.id.activity_main);
-        layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.android_main_context);
+        LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         buttons = new Button[99];
         textViews = new TextView[99];
+        editText = (EditText) findViewById(R.id.edit_text);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+        viewInspector = new ViewInspector(layoutParams, linearLayout,this); //создаем экземпляр нашего класса
+
     }
 
     @Override
@@ -39,18 +50,61 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        ViewInspector viewInspector = new ViewInspector(layoutParams,linearLayout,item);
         switch (item.getItemId())
         {
             case 1:
-                buttons[countOfButtons] = viewInspector.setDefaultViewOptions(new Button(this));
+                buttons[countOfButtons] = viewInspector.setDefaultViewOptions(new Button(this)); //создаем кнопку и назначаем ей начальные найтроки в viewInspector
+                registerForContextMenu(buttons[countOfButtons]);
                 countOfButtons++;
                 break;
+
             case 2:
-                textViews[countOfTextViews] = viewInspector.setDefaultViewOptions(new TextView(this));
+                textViews[countOfTextViews] = viewInspector.setDefaultViewOptions(new TextView(this));// точно также с textView
+                registerForContextMenu(textViews[countOfTextViews]);
                 countOfTextViews++;
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+            try {
+                if (v.getTag().equals(buttons[v.getId()].getTag())) {
+                    checkElement = true;
+                }
+                else {
+                    checkElement = false;
+                }
+            }
+            catch (Exception e) {
+                checkElement = false;
+            }
+        menu.add(0, 1, 0, "Изменить ширину");
+        menu.add(0, 2, 0, "Изменить высоту");
+        menu.add(0, 3, 0, "Изменить текст");
+        menu.add(0, 4 ,0, "Удалить объект");
+        ID = v.getId();
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        viewInspector.setContextOptions(item);
+        editText.setVisibility(View.VISIBLE);
+        button.setVisibility(View.VISIBLE);
+        return super.onContextItemSelected(item);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(!checkElement) {
+
+            textViews[ID] = viewInspector.setPropertiesForView(textViews[ID], editText, button);
+        }
+        else {
+            buttons[ID] = viewInspector.setPropertiesForView(buttons[ID], editText, button);
+        }
     }
 }
--- app/src/main/java/com/development/kernel/draft/ViewInspector.java
+++ app/src/main/java/com/development/kernel/draft/ViewInspector.java
@@ -1,49 +1,140 @@
 package com.development.kernel.draft;
 
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 class ViewInspector {
 
-    private MenuItem item = null;
+
     private LayoutParams viewParams = null;
     private LinearLayout linearLayout = null;
-    private int CountOfButtons = 0;
 
-    ViewInspector(LayoutParams layoutParams, LinearLayout linearLayout, MenuItem item) {
-        this.item = item;
+    private int buttonsId = 0;
+    private int CountButtonsTags = 0;
+
+
+    private int CountTextViewTags = 0;
+    private int textViewsId = 0;
+
+    private int WIS;
+    private MainActivity mainActivity;
+
+    ViewInspector(LayoutParams layoutParams, LinearLayout linearLayout, MainActivity mainActivity) {
         this.viewParams = layoutParams;
         this.linearLayout = linearLayout;
+        this.mainActivity = mainActivity;
     }
 
     Button setDefaultViewOptions(Button button) {  //назначает начальные настройки кнопке и тексту перегружен (2)
         button.setText("New button");
         button.setLayoutParams(viewParams);
-        button.setId(CountOfButtons); //теги и id для массива кнопок
-        button.setTag(CountOfButtons);
+        button.setId(buttonsId); //теги и id для массива кнопок
+        String buttonTags = "Button:";
+        button.setTag(buttonTags + String.valueOf(CountButtonsTags++));
         linearLayout.addView(button);
+        buttonsId++;
         return button;
     }
 
     TextView setDefaultViewOptions(TextView textView) {
         textView.setText("New text");
+        textView.setTextSize(30);
         textView.setLayoutParams(viewParams);
-        textView.setId(CountOfButtons); //теги и id для массива кнопок
-        textView.setTag(CountOfButtons);
+        textView.setId(textViewsId); //теги и id для массива кнопок
+        String textViewsTags = "TextView:";
+        textView.setTag(textViewsTags + String.valueOf(CountTextViewTags++)); //Была создана переменная, чтобы упростить читаемость кода, можно все сделать и с 2 переменными
         linearLayout.addView(textView);
+        textViewsId++;
         return textView;
     }
 
     public ImageView setDefaultViewOptions(ImageView imageView) {
         imageView.setLayoutParams(viewParams);
-        imageView.setId(CountOfButtons); //теги и id для массива кнопок
-        imageView.setTag(CountOfButtons);
         linearLayout.addView(imageView);
         return imageView;
     }
 
+    void setContextOptions(MenuItem item)
+    {
+        WIS = item.getItemId();
+    }
+
+    Button setPropertiesForView(Button button, EditText editText, Button buttonEdit)
+    {
+        switch (WIS)
+        {
+            case 1:
+                try {
+                    button.setWidth(Integer.valueOf(editText.getText().toString()));
+                }
+                catch (Exception e) {
+                    Toast.makeText(mainActivity,"Неверный формат ввода",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 2:
+                try {
+                    button.setHeight(Integer.valueOf(editText.getText().toString()));
+                    editText.setVisibility(View.GONE);
+                }
+                catch (Exception e) {
+                    Toast.makeText(mainActivity,"Неверный формат ввода",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 3:
+                button.setText(editText.getText().toString());
+                editText.setVisibility(View.GONE);
+                break;
+            case 4:
+                button.setVisibility(View.GONE);
+                break;
+
+        }
+        editText.setVisibility(View.GONE);
+        buttonEdit.setVisibility(View.GONE);
+        return button;
+    }
+
+    TextView setPropertiesForView(TextView textView, EditText editText, Button buttonEdit)
+    {
+        switch (WIS)
+        {
+            case 1:
+                try {
+                    textView.setWidth(Integer.valueOf(editText.getText().toString()));
+                }
+                catch (Exception e) {
+                    Toast.makeText(mainActivity,"Неверный формат ввода",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 2:
+                try {
+                    textView.setHeight(Integer.valueOf(editText.getText().toString()));
+                    editText.setVisibility(View.GONE);
+                    textView.setVisibility(View.GONE);
+                }
+                catch (Exception e) {
+                    Toast.makeText(mainActivity,"Неверный формат ввода",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 3:
+                textView.setText(editText.getText().toString());
+                editText.setVisibility(View.GONE);
+                buttonEdit.setVisibility(View.GONE);
+                break;
+            case 4:
+                textView.setVisibility(View.GONE);
+                break;
+
+        }
+        editText.setVisibility(View.GONE);
+        buttonEdit.setVisibility(View.GONE);
+        return textView;
+    }
 }
